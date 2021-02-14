6b2ed4c43eebfbfa3170c45824be9f829558174a
crimson <joojs2004@gmail.com>
2021-02-14 18:21:47 +0900

    add config urls

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
